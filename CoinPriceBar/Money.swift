fa04ac835efa8228075921cfb32d80d94ef88b63
Mads Kleemann <mk@napp.dk>
2018-01-09 11:16:36 +0100

    Added modum from Binance and changed supportedCoins array

--- CoinPriceBar/Money.swift
+++ CoinPriceBar/Money.swift
@@ -9,52 +9,55 @@
 typealias FreedomMoney = Coin
 
 enum Coin: String {
-  case BTC
-  case BCH
-  case ETH
-  case LTC
-  case XRP
-  case IOTA
-  case ADA
+    case BTC
+    case BCH
+    case ETH
+    case LTC
+    case XRP
+    case IOTA
+    case ADA
+    case MOD
 }
 
 enum FiatMoney: String {
-  case USD
+    case USD
 }
 
 extension Coin {
-
-  func priceProvider() -> CoinPriceProvider {
-    switch self {
-    case .XRP, .IOTA, .ADA: return BinancePriceProvider()
-    default: return CoinbasePriceProvider()
+    
+    func priceProvider() -> CoinPriceProvider {
+        switch self {
+        case .XRP, .IOTA, .ADA, .MOD: return BinancePriceProvider()
+        default: return CoinbasePriceProvider()
+        }
     }
-  }
 }
 
 extension Coin {
-
-  func unicode() -> String {
-    switch self {
-    case .BTC: return "₿"
-    case .BCH: return "₿"
-    case .ETH: return "Ξ"
-    case .LTC: return "Ł"
-    case .XRP: return "Ʀ"
-    case .IOTA: return "ι"
-    case .ADA: return "a̶"
+    
+    func unicode() -> String {
+        switch self {
+        case .BTC: return "₿"
+        case .BCH: return "₿"
+        case .ETH: return "Ξ"
+        case .LTC: return "Ł"
+        case .XRP: return "Ʀ"
+        case .IOTA: return "ι"
+        case .ADA: return "a̶"
+        case .MOD: return "m"
+        }
     }
-  }
-
-  func color() -> NSColor {
-    switch self {
-    case .BTC: return NSColor(displayP3Red: 244 / 255, green: 180 / 255, blue: 69 / 255, alpha: 1)
-    case .BCH: return NSColor(displayP3Red: 154 / 255, green: 193 / 255, blue: 98 / 255, alpha: 1)
-    case .ETH: return NSColor(displayP3Red: 115 / 255, green: 123 / 255, blue: 182 / 255, alpha: 1)
-    case .LTC: return NSColor(displayP3Red: 182 / 255, green: 180 / 255, blue: 182 / 255, alpha: 1)
-    case .XRP: return NSColor(displayP3Red: 62 / 255, green: 139 / 255, blue: 192 / 255, alpha: 1)
-    case .IOTA: return NSColor(displayP3Red: 227 / 255, green: 227 / 255, blue: 227 / 255, alpha: 1)
-    case .ADA: return NSColor(displayP3Red: 59 / 255, green: 110 / 255, blue: 207 / 255, alpha: 1)
+    
+    func color() -> NSColor {
+        switch self {
+        case .BTC: return NSColor(displayP3Red: 244 / 255, green: 180 / 255, blue: 69 / 255, alpha: 1)
+        case .BCH: return NSColor(displayP3Red: 154 / 255, green: 193 / 255, blue: 98 / 255, alpha: 1)
+        case .ETH: return NSColor(displayP3Red: 115 / 255, green: 123 / 255, blue: 182 / 255, alpha: 1)
+        case .LTC: return NSColor(displayP3Red: 182 / 255, green: 180 / 255, blue: 182 / 255, alpha: 1)
+        case .XRP: return NSColor(displayP3Red: 62 / 255, green: 139 / 255, blue: 192 / 255, alpha: 1)
+        case .IOTA: return NSColor(displayP3Red: 227 / 255, green: 227 / 255, blue: 227 / 255, alpha: 1)
+        case .ADA: return NSColor(displayP3Red: 59 / 255, green: 110 / 255, blue: 207 / 255, alpha: 1)
+        case .MOD: return NSColor(displayP3Red: 154 / 255, green: 193 / 255, blue: 98 / 255, alpha: 1)
+        }
     }
-  }
 }
